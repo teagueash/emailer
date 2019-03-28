62113964127294f7808d6e84b20a59a3eb2f3955
Teague  Ashcraft <tkashcra@usc.edu>
2019-03-28 14:38:36 -0700

    changed action function signatures to always return

---
+++ actions/dbQuery.js
@@ -0,0 +1,34 @@
+const db = require("../models/dbconnection");
+const validator = require("validator");
+
+const dbQuery = {
+  // query the number of users and return the result
+  getAllUsers: function(callback) {
+    const q = "SELECT COUNT(*) AS count FROM users";
+
+    db.query(q, function(err, result) {
+      if (err) {
+        return callback({ message: "action failed" });
+      }
+      return callback({ message: "action success", count: result[0].count });
+    });
+  },
+  // validate email and then query database for insertion
+  registerUser: function(callback, email) {
+    const validEmail = validator.isEmail(email);
+
+    if (validEmail) {
+      const q = `INSERT INTO users (email) VALUES ('${email}')`;
+      db.query(q, email, function(err, result) {
+        if (err) {
+          return callback({ message: "this user already exists" });
+        }
+        return callback({ message: "action success" });
+      });
+    } else {
+      return callback({ message: "please provide a valid email" });
+    }
+  }
+};
+
+module.exports = dbQuery;
---
+++ actions/mailer.js
@@ -0,0 +1,17 @@
+const nodemailer = require("../models/nodemailer");
+
+const mailer = {
+  sendConfirmationEmail: function(callback, email) {
+    const { transporter, mailOptions } = nodemailer;
+
+    const injectedOptions = { ...mailOptions, to: email };
+    transporter.sendMail(injectedOptions, function(err, info) {
+      if (err) {
+        return callback({ message: "unable to send confirmation email" });
+      }
+      return callback({ message: "successfully sent confirmation email" });
+    });
+  }
+};
+
+module.exports = mailer;
